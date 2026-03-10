74e376085cdf6d1b6c3e27cd7885c87423767a3b
loegcas <110066175+4shizzgndlf@users.noreply.github.com>
2026-03-10 14:31:30 +0100

    Added Welcome message

--- src/main/java/dk/ek/countries/controllers/WelcomeController.java
+++ src/main/java/dk/ek/countries/controllers/WelcomeController.java
@@ -1,13 +1,19 @@
 package dk.ek.countries.controllers;
 
+import dk.ek.countries.services.WelcomeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class WelcomeController {
+    private WelcomeService service = new WelcomeService();
 
     @GetMapping("/")
-    public String welcome() {
-        return "Welcome to the countries website!";
+    public ModelAndView welcome() {
+        ModelAndView mav = new ModelAndView("index");
+        String message = service.getWelcome();
+        mav.addObject("welcome", message);
+        return mav;
     }
 }
---
+++ src/main/java/dk/ek/countries/services/WelcomeService.java
@@ -0,0 +1,8 @@
+package dk.ek.countries.services;
+
+public class WelcomeService {
+    public String getWelcome() {
+        String message = "Welcome to the countries website, everyone!";
+        return message;
+    }
+}
